4500bfc35c330eae034a3da76cb1024eb5f1ee5c
Anton Antipov <managerantonantipov@yandex.ru>
2022-10-25 22:08:36 +0700

    bagfix:исправить все замечания ревьюера

--- script/index.js
+++ script/index.js
@@ -2,21 +2,23 @@ const popup = document.querySelector('.popup');
 const openButton = document.querySelector('.profile-info__edit-button_open-popup');
 const closeButton = document.querySelector('.popup__close');
 
+
 const openPopup = function(){
     popup.classList.add('popup__opened');
 }
-openButton.addEventListener('click',openPopup)
+openButton.addEventListener('click', openPopup)
 
 const closePopup = function(){
     popup.classList.remove('popup__opened');
+    formElement.reset();
 }
 closeButton.addEventListener('click', closePopup)
 
-let formElement = document.querySelector('.popup__container');
-let nameImput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-about');
+const formElement = document.querySelector('.popup__container');
+const nameImput = document.querySelector('.popup__input-name');
+const jobInput = document.querySelector('.popup__input-about');
 
-function formSubmitHandler(evt){
+function editProfile(evt){
     evt.preventDefault();
   const heading = document.querySelector ('.profile-info__name');
   heading.textContent = nameImput.value 
@@ -24,5 +26,5 @@ function formSubmitHandler(evt){
   aboutInfo.textContent = jobInput.value  
 
 }
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', editProfile);
 formElement.addEventListener('submit', closePopup); 
